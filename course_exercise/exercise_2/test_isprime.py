b42258ce4fe6b74ea178192481a7223bc6356aeb
liangxinzhu <liangxinzhu1998@163.com>
2021-05-26 17:01:29 +0100

    delete

--- course_exercise/exercise_2/test_isprime.py
+++
@@ -1,8 +0,0 @@
-from math_utils import isprime
-
-
-def test_isprime_value():
-    for i, f in enumerate([2, 19, 23, 97]):
-        assert isprime(f) == 'True'
-    for i, f in enumerate([15, 20, 34, 50]):
-        assert isprime(f) == 'False'

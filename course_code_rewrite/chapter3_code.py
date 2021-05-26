5dbd58539378f44938f2b961b73a6c22635bd9f9
liangxinzhu <liangxinzhu1998@163.com>
2021-05-26 18:31:52 +0100

    deal with flake8 problem

--- course_code_rewrite/chapter3_code.py
+++ course_code_rewrite/chapter3_code.py
@@ -1,3 +1,5 @@
+from numbers import Number
+
 a = 1
 b = 2
 print(a + b)
@@ -14,11 +16,16 @@ type(1)
 type(abs)
 type(int)
 #
+
+
 class Polynomial:
+
     def __init__(self, coefs):
         self.coefficients = coefs
+
     def degree(self):
         return len(self.coefficients) - 1
+
     def __str__(self):
         coefs = self.coefficients
         terms = []
@@ -28,20 +35,25 @@ class Polynomial:
         if self.degree() > 0 and coefs[1]:
             terms.append(f"{coefs[1]}x")
         # Remaining terms look like cx^d, though factors of a are dropped.
+
         terms += [f"{'' if c == 1 else c}x^{d}"
-                  for d, c in enumerate(coefs[2:], start = 2) if c]
+                  for d, c in enumerate(coefs[2:], start=2) if c]
         # Sum polynomial terms from high to low exponent.
         return " + ".join(reversed(terms)) or "0"
+
     def __repr__(self):
         return self.__class__.__name__ + "(" + repr(self.coefficients) + ")"
+
     def __eq__(self, other):
         return isinstance(other, Polynomial) and \
             self.coefficients == other.coefficients
+
     def __add__(self, other):
         if isinstance(other, Number):
-            return Polynomial((self.coefficients[0] + other,) + self.coefficients[1:])
+            return Polynomial((self.coefficients[0] + other,)
+                              + self.coefficients[1:])
         elif isinstance(other, Polynomial):
-            ...#there is some lines
+            ...  # there is some lines
 
 
 f = Polynomial((0, 1, 2))
@@ -55,11 +67,11 @@ print(f)
 (0, 1, "f") == (0, 1 + 0j, 'f')
 (0, 1, "f") == (0., 1 + 0j, 'g')
 (0, 1, "f") == (0., 1 + 0j)
-# 
-from example_code.polynomial import Polynomial 
+#
+from example_code.polynomial import Polynomial # noqa F401 
 a = Polynomial((1, 0, 1))
 b = Polynomial((1, 0, 1))
-a == b 
+a == b
 #
 id(a)
 id(b)
